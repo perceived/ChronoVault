8f9b7fb6fa4abfbb036aebf7cb00b3a01f6730ab
Ryan Clark <rclark@chronosapien.com>
2017-06-28 12:32:50 -0400

    Added SmartEnum
    Added SmartEnum, an architecture tool for creating enumerations with generic values.

---
+++ Architecture Tools/Enumeration/SmartEnum.cs
@@ -0,0 +1,537 @@
+﻿/**
+	SmartEnum:
+		A 'smart enum' pattern, where the value of the enumeration is a generic comparable struct (which should be immutable).
+		Enum values are declared as 'public static readonly' fields in the inheriting class, and can be enumerated over using static and instance methods.
+	Author(s):
+		Ryan Scott Clark
+	Date Created:
+		06-20-2017
+	Date Last Modified:
+		06-27-2017
+	Attach Script to:
+		
+	Notes:
+		Uses reflection for populating the collection.
+	Change log:
+		06-26-2017:	Code cleanup.
+						Added GenerateCollection and moved collection genereration code out of other methods.
+						Removed the private Enumerate with a Type parameter, since that was only needed for collection generation.
+						Added GetListInternal for handling direct list access.
+					Added Query methods: Count, Contains, Find, TryGetDeclared.
+						Renamed staticReference to declaredReference.
+					Added IComparable<T>, IEquatable<T>, and SmartEnum<T> and T comparison operators.
+	To do:
+		* Option where runtime created instances with unique values can be added to the Enumerate collection (or, more likely, a separate mixed collection).
+		* Enum methods:
+			- [Complete]
+		* EnumUtility methods:
+			- static bool isValid — Determines if runtime declared values are valid
+	Bugs:
+		
+**/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace ChronoVault {
+
+	/// <summary>
+	///		A 'smart enum' pattern, where the value of the enumeration is a generic comparable struct (which should be immutable).
+	///		Enum values are declared as 'public static readonly' fields in the inheriting class, and can be enumerated over using static and instance methods.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public abstract class SmartEnum<T> : IComparable, IComparable<SmartEnum<T>>, IComparable<T>, IEquatable<SmartEnum<T>>, IEquatable<T>
+		where T : struct, IComparable, IComparable<T>, IEquatable<T> {
+		
+		/// <summary>
+		///		The value to use for the enumeration entry.
+		/// </summary>
+		public T value {
+			get {
+				return _value;
+			}
+		}
+		[SerializeField]
+		private T _value;
+
+		/// <summary>
+		///		The display name for the value.
+		/// </summary>
+		public string name {
+			get {
+				return _name;
+			}
+		}
+		[HideInInspector, SerializeField]
+		private string _name;
+
+		/// <summary>
+		///		Stores a reference to a matching statically declared field.  This is used to get property values on comparable types.
+		/// </summary>
+		protected SmartEnum<T> declaredReference {
+			get {
+				#if UNITY_EDITOR
+				if (!Application.isPlaying || (!staticReferenceChecked && object.ReferenceEquals(_declaredReference, null))) {
+				#else
+				if (!staticReferenceChecked && object.ReferenceEquals(_staticReference, null)) {
+				#endif
+					staticReferenceChecked = true;
+
+					foreach (SmartEnum<T> val in GetListInternal(GetType())) {
+						if (val.Equals(this)) {
+							_declaredReference = val;
+							return _declaredReference;
+						}
+					}
+
+					//	For Editor sake
+					_declaredReference = null;
+				}
+				return _declaredReference;
+			}
+		}
+		private SmartEnum<T> _declaredReference;
+		private bool staticReferenceChecked;
+
+
+
+		/// <summary>
+		///		Stores Enumerate values.
+		///		
+		///		<para> Key: Type of the SmartEnum&lt;T&gt;. </para>
+		///		<para> Value: List of all statically declared fields for the SmartEnum, sorted by increasing value order. </para>
+		/// </summary>
+		private static Dictionary<Type, List<SmartEnum<T>>> enumerateDictionary = new Dictionary<Type, List<SmartEnum<T>>>();
+
+
+
+		public override string ToString() {
+
+			return name;
+		}
+
+		#region Enumerate methods (public)
+		/// <summary>
+		///		An enumerable collection of all statically declared fields for the SmartEnum, sorted by increasing value order.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public static IEnumerable<U> Enumerate<U>() where U : SmartEnum<T> {
+
+			foreach (U val in GetListInternal(typeof(U)))
+				yield return val as U;
+		}
+
+		/// <summary>
+		///		A list of all statically declared fields for the SmartEnum, sorted by increasing value order.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public static List<U> GetList<U>() where U : SmartEnum<T> {
+			
+			return GetListInternal(typeof(U)).Cast<U>().ToList();
+		}
+
+		/// <summary>
+		///		Starts an increasing enumeration from the value greater than this.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public IEnumerable<U> EnumerateFrom<U>() where U : SmartEnum<T> {
+
+			bool reached = false;
+
+			//	Case where we match a static field
+			if (!object.ReferenceEquals(declaredReference, null)) {
+				foreach (U val in Enumerate<U>()) {
+					if (!reached) {
+						if (val.Equals(this))
+							reached = true;
+					}
+					else
+						yield return val;
+				}
+			}
+			//	Case where we have a custom value
+			else {
+				foreach (U val in Enumerate<U>()) {
+					if (!reached) {
+						if (val > this) {
+							reached = true;
+							yield return val;
+						}
+					}
+					else
+						yield return val;
+				}
+			}
+			yield break;
+		}
+
+		/// <summary>
+		///		Starts an increasing enumeration from the value greater than this.
+		///		NOTE: Syntactic sugar for EnumerateFrom().
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public IEnumerable<U> IncrementFrom<U>() where U : SmartEnum<T> {
+
+			foreach (U val in EnumerateFrom<U>())
+				yield return val;
+		}
+
+		/// <summary>
+		///		Starts a decreasing enumeration from the value less than this.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public IEnumerable<U> DecrementFrom<U>() where U : SmartEnum<T> {
+			
+			bool reached = false;
+
+			//	Case where we match a static field
+			if (!object.ReferenceEquals(declaredReference, null)) {
+				foreach (U val in Enumerate<U>().Reverse()) {
+					if (!reached) {
+						if (val.Equals(this))
+							reached = true;
+					}
+					else
+						yield return val;
+				}
+			}
+			//	Case where we have a custom value
+			else {
+				foreach (U val in Enumerate<U>().Reverse()) {
+					if (!reached) {
+						if (val < this) {
+							reached = true;
+							yield return val;
+						}
+					}
+					else
+						yield return val;
+				}
+			}
+			yield break;
+		}
+
+		/// <summary>
+		///		Returns the next largest value from this.
+		///		Will be null if there are no larger elements.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public U Next<U>() where U : SmartEnum<T> {
+
+			return EnumerateFrom<U>().FirstOrDefault();
+		}
+
+		/// <summary>
+		///		Returns the next smallest value from this.
+		///		Will be null if there are no smaller elements.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public U Prior<U>() where U : SmartEnum<T> {
+
+			return DecrementFrom<U>().FirstOrDefault();
+		}
+		#endregion
+
+		#region Query methods (public)
+		/// <summary>
+		///		Gets the number of statically declared fields for the SmartEnum.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <returns></returns>
+		public static int Count<U>() where U : SmartEnum<T> {
+
+			return GetListInternal(typeof(U)).Count();
+		}
+
+		/// <summary>
+		///		Determines whether an element is one of the statically declared fields for the SmartEnum.
+		///		NOTE: This checks equality of the 'value' property, not reference equality of the SmartEnum instance.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static bool Contains<U>(U value) where U : SmartEnum<T> {
+
+			return GetListInternal(typeof(U)).Exists(val => val.Equals(value));
+		}
+
+		/// <summary>
+		///		Determines whether a value matches one of the statically declared fields' values for the SmartEnum.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static bool Contains<U>(T value) where U : SmartEnum<T> {
+
+			return GetListInternal(typeof(U)).Exists(val => val.value.Equals(value));
+		}
+
+		/// <summary>
+		///		Searches for a statically declared field that matches the predicate.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="match"></param>
+		/// <returns></returns>
+		public static U Find<U>(Predicate<U> match) where U : SmartEnum<T> {
+
+			foreach (U val in GetListInternal(typeof(U)))
+				if (match(val))
+					return val;
+			return default(U);
+		}
+
+		/// <summary>
+		///		Searches for a statically declared field that matches the predicate, by value.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="match"></param>
+		/// <returns></returns>
+		public static U Find<U>(Predicate<T> match) where U : SmartEnum<T> {
+
+			return Find<U>((U val) => match(val.value));
+		}
+
+		/// <summary>
+		///		Searches for a statically declared field that matches the value.
+		///		NOTE: Syntactic sugar for TryGetDeclared.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static U Find<U>(U value) where U : SmartEnum<T> {
+
+			U match;
+			TryGetDeclared(value, out match);
+			return match;
+		}
+
+		/// <summary>
+		///		Searches for a statically declared field that matches the value.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static U Find<U>(T value) where U : SmartEnum<T> {
+
+			return Find<U>(val => val.value.Equals(value));
+		}
+
+		/// <summary>
+		///		Gets the statically declared reference that matches an instance, if one exists.
+		/// </summary>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="instance"></param>
+		/// <param name="declared"></param>
+		/// <returns></returns>
+		public static bool TryGetDeclared<U>(U instance, out U declared) where U : SmartEnum<T> {
+
+			if (object.ReferenceEquals(instance, null) || object.ReferenceEquals(instance.declaredReference, null)) {
+				declared = null;
+				return false;
+			}
+
+			declared = instance.declaredReference as U;
+			return true;
+		}
+		#endregion
+
+		#region Collection methods (Private)
+		/// <summary>
+		///		Generates a collection for the specified Type in enumerateDictionary.
+		///		For internal use only—there are no Type or Dictionary exists checks.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		private static List<SmartEnum<T>> GenerateCollection(Type type) {
+
+			List<SmartEnum<T>> values = new List<SmartEnum<T>>();
+			foreach (FieldInfo info in type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)) {
+				object value = info.GetValue(null);
+				if (value != null && value.GetType() == type)
+					values.Add((SmartEnum<T>) value);
+			}
+
+			values.Sort();
+			enumerateDictionary.Add(type, values);
+			return values;
+		}
+
+		/// <summary>
+		///		Directly returns the list internally used in enumerateDictionary for the specified Type.
+		///		For internal use only—the actual list is returned, not a copy.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		private static List<SmartEnum<T>> GetListInternal(Type type) {
+
+			List<SmartEnum<T>> values;
+			if (!enumerateDictionary.TryGetValue(type, out values))
+				values = GenerateCollection(type);
+			return values;
+		}
+		#endregion
+
+		#region Comparison operators, SmartEnum
+		public static bool operator <(SmartEnum<T> left, SmartEnum<T> right) {
+
+			return !object.ReferenceEquals(left, right) && (object.ReferenceEquals(left, null) || left.CompareTo(right) < 0);
+		}
+
+		public static bool operator >(SmartEnum<T> left, SmartEnum<T> right) {
+
+			return !object.ReferenceEquals(left, right) && (!object.ReferenceEquals(left, null) && left.CompareTo(right) > 0);
+		}
+
+		public static bool operator <=(SmartEnum<T> left, SmartEnum<T> right) {
+
+			return object.ReferenceEquals(left, right) || object.ReferenceEquals(left, null) || left.CompareTo(right) <= 0;
+		}
+
+		public static bool operator >=(SmartEnum<T> left, SmartEnum<T> right) {
+
+			return object.ReferenceEquals(left, right) || (!object.ReferenceEquals(left, null) && left.CompareTo(right) >= 0);
+		}
+
+		public static bool operator ==(SmartEnum<T> left, SmartEnum<T> right) {
+
+			return object.ReferenceEquals(left, right) || (!object.ReferenceEquals(left, null) && left.Equals(right));
+		}
+
+		public static bool operator !=(SmartEnum<T> left, SmartEnum<T> right) {
+
+			return !object.ReferenceEquals(left, right) && (object.ReferenceEquals(left, null) || !left.Equals(right));
+		}
+		#endregion
+
+		#region Comparison operators, T
+		public static bool operator <(SmartEnum<T> left, T right) {
+
+			return object.ReferenceEquals(left, null) || left.CompareTo(right) < 0;
+		}
+
+		public static bool operator <(T left, SmartEnum<T> right) {
+
+			return right > left;
+		}
+
+		public static bool operator >(SmartEnum<T> left, T right) {
+
+			return !object.ReferenceEquals(left, null) && left.CompareTo(right) > 0;
+		}
+
+		public static bool operator >(T left, SmartEnum<T> right) {
+
+			return right < left;
+		}
+
+		public static bool operator <=(SmartEnum<T> left, T right) {
+
+			return object.ReferenceEquals(left, null) || left.CompareTo(right) <= 0;
+		}
+
+		public static bool operator <=(T left, SmartEnum<T> right) {
+
+			return right >= left;
+		}
+
+		public static bool operator >=(SmartEnum<T> left, T right) {
+
+			return !object.ReferenceEquals(left, null) && left.CompareTo(right) >= 0;
+		}
+
+		public static bool operator >=(T left, SmartEnum<T> right) {
+
+			return right <= left;
+		}
+
+		public static bool operator ==(SmartEnum<T> left, T right) {
+
+			return !object.ReferenceEquals(left, null) && left.value.Equals(right);
+		}
+
+		public static bool operator ==(T left, SmartEnum<T> right) {
+
+			return right == left;
+		}
+
+		public static bool operator !=(SmartEnum<T> left, T right) {
+
+			return object.ReferenceEquals(left, null) || !left.value.Equals(right);
+		}
+
+		public static bool operator !=(T left, SmartEnum<T> right) {
+
+			return right != left;
+		}
+		#endregion
+
+		#region Comparison methods
+		public int CompareTo(object obj) {
+
+			if (obj == null)
+				return 1;
+
+			if (obj is SmartEnum<T>)
+				return CompareTo((SmartEnum<T>) obj);
+			if (obj is T)
+				return CompareTo((T) obj);
+			throw new ArgumentException("Passed obj is not a SmartEnum<T>.");
+		}
+
+		public virtual int CompareTo(SmartEnum<T> other) {
+
+			return object.ReferenceEquals(other, null) ? 1 : value.CompareTo(other.value);
+		}
+
+		public virtual int CompareTo(T other) {
+
+			return value.CompareTo(other);
+		}
+
+		public override bool Equals(object obj) {
+
+			return !object.ReferenceEquals(obj, null) && ((obj is SmartEnum<T> && Equals((SmartEnum<T>) obj)) || (obj is T && Equals((T) obj)));
+		}
+
+		public bool Equals(SmartEnum<T> other) {
+
+			return !object.ReferenceEquals(other, null) && value.Equals(other.value);
+		}
+
+		public bool Equals(T other) {
+
+			return value.Equals(other);
+		}
+
+		public override int GetHashCode() {
+
+			return value.GetHashCode();
+		}
+		#endregion
+
+
+
+		/// <summary>
+		///		Creates a SmartEnum with the provided value and name.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="name"></param>
+		protected SmartEnum(T value, string name) {
+
+			_value = value;
+			_name = name;
+		}
+
+		protected SmartEnum() {}
+	}
+}
